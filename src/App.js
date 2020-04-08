fc2cac82acdb8b11239c15cab74c3a905f47d846
Thomas Marchand <thomas.marchand@tuta.io>
2020-04-08 23:16:47 +0200

    :sparkles: updates default node address

--- src/App.js
+++ src/App.js
@@ -93,7 +93,7 @@ class App extends React.Component {
 
         <div className="px-4 pb-4">
           <label htmlFor="node_address" className="text-sm block font-bold  pb-2">Node address</label>
-          <input name="node_address" value={this.state.node_address} onChange={this.handleUrlChange} type="text" placeholder="localhost"
+          <input name="node_address" value={this.state.node_address} onChange={this.handleUrlChange} type="text" placeholder="localhost:8080"
             className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
         </div>
 
