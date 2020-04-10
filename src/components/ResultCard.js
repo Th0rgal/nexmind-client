8a0cdaf70426ae0166dc923e68cf89a62c7c3866
Thomas Marchand <thomas.marchand@tuta.io>
2020-04-10 21:34:22 +0200

    :sparkles: add logo

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { ReactComponent as ImageIcon } from "../icons/image.svg";
+
 class ResultCard extends React.Component {
 
     constructor(props) {
@@ -18,6 +20,11 @@ class ResultCard extends React.Component {
     render() {
         return (
             <div className="max-w-sm rounded overflow-hidden shadow-lg">
+
+                <div className="bg-gray-900">
+                    <ImageIcon  className="w-full fill-current text-gray-700" />
+                </div>
+
                 <div className="px-6 py-4">
                     <div className="font-bold text-xl mb-2">{this.state.name}</div>
                     <p className="text-gray-700 text-base">
