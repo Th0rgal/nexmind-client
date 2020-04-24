120ae6df22abdfd573f6766969a15a760341da73
Enminca <emma.esquirol2002@gmail.com>
2020-04-24 19:28:04 +0000

    :sparkles: improve download logo

--- src/components/panel/forms/OpenDataForm.js
+++ src/components/panel/forms/OpenDataForm.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
+
 class OpenDataForm extends React.Component {
 
     handleSubmit = (event) => {
