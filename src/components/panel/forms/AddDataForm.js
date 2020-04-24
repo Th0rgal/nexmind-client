f83d62bfd32bdea9e93cc8c70addbe0e188f4298
Enminca <emma.esquirol2002@gmail.com>
2020-04-24 19:06:27 +0000

    :sparkles: add pencil logo

--- src/components/panel/forms/AddDataForm.js
+++ src/components/panel/forms/AddDataForm.js
@@ -66,9 +66,9 @@ class AddDataForm extends React.Component {
         return (
             <div id="smoke" className="animated fadeIn fixed z-50 pin overflow-auto bg-smoke-dark flex justify-center inset-0">
                 <div className="animated fadeInUp fixed shadow-inner max-w-md md:relative pin-b pin-x align-top m-auto justify-end p-8 bg-white md:rounded w-full md:h-auto md:shadow flex flex-col">
-                    <CloseIcon className="flex text-gray-800 fill-current object-right-bottom" />
 
                     <div className="flex flex-col items-center m-2">
+                        <CloseIcon onClick={this.props.close} className="cursor-pointer text-gray-600 fill-current m-2 h-10 w-10" />
                         <Dropzone onFilesAdded={(files_array) => this.setFile(files_array[0])} />
                     </div>
                     <form onSubmit={this.handleSubmit} >
