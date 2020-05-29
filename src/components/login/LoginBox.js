38aa4c6127790b724b006146a409ade3b8d15ded
Th0rgal <thomas.marchand@tuta.io>
2020-05-29 13:45:09 +0200

    :construction: removing tailwindcss

--- src/components/login/LoginBox.js
+++ src/components/login/LoginBox.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import RequestsManager from '../../utils/RequestsManager'
+import styles from "../../styles/login-box.module.css"
 
 class LoginBox extends React.Component {
 
@@ -55,32 +56,30 @@ class LoginBox extends React.Component {
 
     render() {
         return (
-            <div className="flex flex-col content-center items-center">
+            <div className={styles.content}>
                 <img src={process.env.PUBLIC_URL + '/logo.svg'} className="mx-auto w-64" alt="logo" />
 
-                <form onSubmit={this.handleLogin} className="bg-white shadow-md rounded-lg px-8 py-8" >
+                <form onSubmit={this.handleLogin} className={styles.box} >
 
-                    <div className="px-4 pb-4">
-                        <label htmlFor="node_address" className="font-sans font-bold block text-gray-800 pb-2">Node address</label>
+                    <div className={styles.field}>
+                        <label htmlFor="node_address" className={styles.label}>Node address</label>
                         <input name="node_address" value={this.state.node_address} onChange={this.handleUrlChange} type="text" placeholder="localhost:8080"
-                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
+                            className={styles.input} />
                     </div>
 
-                    <div className="px-4 pb-4">
-                        <label htmlFor="username" className="font-sans block font-bold text-gray-800 pb-2">Username</label>
+                    <div className={styles.field}>
+                        <label htmlFor="username" className={styles.label}>Username</label>
                         <input name="username" value={this.state.username} onChange={this.handleChange} type="text" placeholder="username"
-                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
+                            className={styles.input} />
                     </div>
 
-                    <div className="px-4 pb-4">
-                        <label htmlFor="password" className="font-sans block font-bold text-gray-800 pb-2">Password</label>
+                    <div className={styles.field}>
+                        <label htmlFor="password" className={styles.label}>Password</label>
                         <input name="password" value={this.state.password} onChange={this.handleChange} type="password" placeholder="password"
-                            className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-blue-500" />
+                            className={styles.input} />
                     </div>
 
-                    <div className="p-4 px-4 pb-4">
-                        <button className="hover:bg-transparent bg-blue-500 hover:text-blue-700 text-white py-2 px-4 border hover:border-blue-500 hover:border-transparent rounded-full w-full" >Sign In</button>
-                    </div>
+                    <button className={styles.submit} >Sign In</button>
                 </form>
             </div>
         )
